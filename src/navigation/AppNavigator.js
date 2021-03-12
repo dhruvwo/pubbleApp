705182aa055915b5c09d215795c5133bfed59f26
Dhruv WO <dhruv.webosmotic@gmail.com>
2021-03-12 10:32:37 +0530

    - added bottom tabs - added env variable - updated ios icon - added splash screen for ios

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -2,10 +2,12 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {navigationRef, isReadyRef} from './RootNavigation';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 /* Import screen js files */
 import Login from '../screens/Login';
+import MyTabBar from '../screens/MyTabBar';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -26,25 +28,51 @@ function Messages() {
   );
 }
 
-function Home() {
+const tabs = [
+  {
+    name: 'QuickPay',
+    component: Feed,
+  },
+  {
+    name: 'Products',
+    component: Messages,
+  },
+];
+
+function BottomTab() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Feed" component={Feed} />
-      <Tab.Screen name="Messages" component={Messages} />
+    <Tab.Navigator
+      tabBar={(props) => <MyTabBar {...props} />}
+      tabBarOptions={{
+        showLabel: false,
+      }}>
+      {tabs.map((tab) => {
+        return (
+          <Tab.Screen
+            key={tab.name}
+            name={tab.name}
+            component={tab.component}
+          />
+        );
+      })}
     </Tab.Navigator>
   );
 }
 
 export default function AppNavigator() {
   return (
-    <NavigationContainer>
+    <NavigationContainer
+      ref={navigationRef}
+      onReady={() => {
+        isReadyRef.current = true;
+      }}>
       <Stack.Navigator>
         <Stack.Screen
           name="Login"
           component={Login}
           options={{headerShown: false}}
         />
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={BottomTab} />
       </Stack.Navigator>
     </NavigationContainer>
   );
