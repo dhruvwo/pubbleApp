07cc8f1ccb80e64f4c0586914c5bd5a8d7472bfb
Dhruv WO <dhruv.webosmotic@gmail.com>
2021-03-31 11:10:49 +0530

    updated groups & users collection were not updating properly

--- src/store/reducers/collections.js
+++ src/store/reducers/collections.js
@@ -1,4 +1,5 @@
 import {CollectionsState} from '../../constants/GlobalState';
+import * as _ from 'lodash';
 
 const initialState = {
   users: {},
@@ -13,19 +14,30 @@ export const collections = (state = initialState, action) => {
         initData: action.data,
       };
     case CollectionsState.SET_USER_COLLECTION:
+      let users = state.users;
+      if (_.isEmpty(users)) {
+        users = action.data;
+      } else {
+        _.forIn(users, (user, key) => {
+          users[key] = user;
+        });
+      }
       return {
         ...state,
-        users: action.data,
+        users,
       };
     case CollectionsState.SET_GROUP_COLLECTION:
+      let groups = state.groups;
+      if (_.isEmpty(groups)) {
+        groups = action.data;
+      } else {
+        _.forIn(groups, (group, key) => {
+          groups[key] = group;
+        });
+      }
       return {
         ...state,
-        groups: action.data,
-      };
-    case CollectionsState.UPDATE_COLLECTION:
-      return {
-        ...state,
-        users: action.data,
+        groups,
       };
     default:
       return state;
