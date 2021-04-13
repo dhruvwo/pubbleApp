04987960a13e2b6e83205b7589c9cdc54b9fdfb1
Dhruv <dhruv.webosmotic@gmail.com>
2021-04-13 17:09:30 +0530

    event detail tag filter dynamic data with search & UI

--- src/components/EventFilter.js
+++ src/components/EventFilter.js
@@ -7,6 +7,7 @@ import * as _ from 'lodash';
 import {eventsAction} from '../store/actions';
 import {useDispatch, useSelector} from 'react-redux';
 import {TouchableOpacity} from 'react-native-gesture-handler';
+import {InputItem} from '@ant-design/react-native';
 
 export default function EventFilter(props) {
   const dispatch = useDispatch();
@@ -15,6 +16,7 @@ export default function EventFilter(props) {
     selectedEvent: auth.selectedEvent,
   }));
   const [tagFilterData, setTagFilterData] = useState([]);
+  const [tagSearch, setTagSearch] = useState('');
 
   useEffect(() => {
     getTagFilterData();
@@ -44,12 +46,79 @@ export default function EventFilter(props) {
       searchAppIds: reduxState.selectedEvent.id,
       statuses: '0,10,20,30,40,50,60',
     };
-    const res = await dispatch(eventsAction.getStreamData(params));
+    await dispatch(eventsAction.getStreamData(params));
+    props.navigation.navigate('Events');
+  }
+
+  const clearSearchInputValue = () => {
+    setTagSearch('');
+  };
+
+  const onChangeSearch = (value) => {
+    setTagSearch(value);
+  };
+
+  async function onSearchHandler() {
+    const params = {
+      communityId: reduxState.communityId,
+      postTypes: 'Q',
+      searchString: tagSearch,
+      scope: 'all',
+      searchAppIds: reduxState.selectedEvent.id,
+      statuses: '10,20,40,50,60,0,30',
+    };
+    await dispatch(eventsAction.getStreamData(params));
     props.navigation.navigate('Events');
   }
 
   return (
     <View style={styles.contentContainer}>
+      <View style={styles.searchMainContainer}>
+        <View style={styles.searchLeftIcon}>
+          <CustomIconsComponent
+            color={Colors.primaryActive}
+            name={'search1'}
+            type={'AntDesign'}
+            size={20}
+          />
+        </View>
+
+        <View style={styles.searchInputContainer}>
+          <InputItem
+            extra={
+              <TouchableOpacity
+                style={styles.searchRightIcon}
+                onPress={clearSearchInputValue}>
+                <CustomIconsComponent
+                  color={'#89A382'}
+                  name={'cross'}
+                  type={'Entypo'}
+                  size={20}
+                />
+              </TouchableOpacity>
+            }
+            placeholder="Search..."
+            placeholderTextColor={'#89A382'}
+            accessible={true}
+            autoCapitalize="none"
+            autoCorrect={false}
+            style={styles.searchInput}
+            onChangeText={(search) => onChangeSearch(search)}
+            value={tagSearch}></InputItem>
+        </View>
+
+        <TouchableOpacity
+          containerStyle={styles.tagAddButton(tagSearch)}
+          onPress={onSearchHandler}>
+          <CustomIconsComponent
+            color={'white'}
+            name={'check'}
+            type={'Entypo'}
+            size={20}
+          />
+        </TouchableOpacity>
+      </View>
+
       <Text style={styles.activityPubbleUsersText}>Tag filter</Text>
 
       <View style={styles.dividerStyleMainContainer}>
@@ -62,34 +131,17 @@ export default function EventFilter(props) {
       </Text>
 
       <View style={styles.tagFilterMainContainer}>
-        {tagFilterData?.data?.map((tags) => (
+        {tagFilterData?.data?.map((tags, index) => (
           <TouchableOpacity
+            key={index}
             onPress={() => onClickTagHandler(tags.tag.name)}
-            style={styles.tagFilterTouchable}>
+            style={styles.tagFilterTouchable(tags.tag.color)}>
             <View style={styles.tagFilterContainer}>
-              <Text
-                style={{
-                  color:
-                    tags.tag.color !== '' ? tags.tag.color : Colors.primaryText,
-                  textAlign: 'center',
-                  flexWrap: 'wrap',
-                }}>
+              <Text style={styles.tagNameText(tags.tag.color)}>
                 {tags.tag.name}
               </Text>
-              <View
-                style={{
-                  marginHorizontal: 5,
-                  borderRightWidth: 1,
-                  borderRightColor:
-                    tags.tag.color !== '' ? tags.tag.color : Colors.primaryText,
-                }}></View>
-              <Text
-                style={{
-                  color:
-                    tags.tag.color !== '' ? tags.tag.color : Colors.primaryText,
-                  textAlign: 'center',
-                  flexWrap: 'wrap',
-                }}>
+              <View style={styles.tagRightDivider(tags.tag.color)}></View>
+              <Text style={styles.tagFiltterCount(tags.tag.color)}>
                 {tags.count}
               </Text>
             </View>
@@ -135,16 +187,64 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
   },
-  tagFilterTouchable: {
+  tagFilterTouchable: (tagColor) => ({
     borderWidth: 2,
-    borderColor: tags.tag.color !== '' ? tags.tag.color : Colors.primaryText,
+    borderColor: tagColor !== '' ? tagColor : Colors.primaryText,
     borderRadius: 28,
     paddingVertical: 5,
     paddingHorizontal: 10,
     marginLeft: 8,
     marginBottom: 10,
-  },
+  }),
   tagFilterContainer: {
     flexDirection: 'row',
   },
+  tagNameText: (tagColor) => ({
+    color: tagColor !== '' ? tagColor : Colors.primaryText,
+    textAlign: 'center',
+    flexWrap: 'wrap',
+  }),
+  tagRightDivider: (tagColor) => ({
+    marginHorizontal: 5,
+    borderRightWidth: 1,
+    borderRightColor: tagColor !== '' ? tagColor : Colors.primaryText,
+  }),
+  tagFiltterCount: (tagColor) => ({
+    color: tagColor !== '' ? tagColor : Colors.primaryText,
+    textAlign: 'center',
+    flexWrap: 'wrap',
+  }),
+
+  searchMainContainer: {
+    borderColor: Colors.primaryInactive,
+    borderWidth: 2,
+    borderRadius: 4,
+    width: '90%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  searchLeftIcon: {
+    paddingLeft: 10,
+    flexDirection: 'row',
+  },
+  searchInputContainer: {
+    width: '94%',
+    paddingRight: 0,
+  },
+  searchRightIcon: {
+    height: '100%',
+    alignItems: 'center',
+    paddingTop: 10,
+  },
+  searchInput: {
+    borderBottomWidth: 0,
+    width: '10%',
+  },
+  tagAddButton: (tagSearch) => ({
+    backgroundColor: Colors.green,
+    padding: 5,
+    borderRadius: 5,
+    opacity: tagSearch ? 1 : 0.5,
+  }),
 });
