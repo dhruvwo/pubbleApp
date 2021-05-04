0cd23ee1a06e08dd9a759f52ea776165a4265ee2
dhruvwo <dhruv.webosmotic@gmail.com>
2021-05-04 18:31:29 +0530

    assign/unassign traslation state update issue resolved

--- src/store/reducers/events.js
+++ src/store/reducers/events.js
@@ -361,14 +361,17 @@ export const events = (state = initialState, action) => {
     case EventsState.SOCKET_UPDATE_CURRENT_STREAM: {
       let currentCardData_UPDATE_SOCKET_EVENT;
       if (state.currentCard.id === action.data.id) {
-        if (action.data?.metadata?.action) {
+        if (
+          action.data?.metadata?.action === 'star' ||
+          action.data?.metadata?.action === 'unstar'
+        ) {
           currentCardData_UPDATE_SOCKET_EVENT = state.currentCard;
           currentCardData_UPDATE_SOCKET_EVENT['star'] =
             action.data.metadata.action === 'star';
         } else {
           currentCardData_UPDATE_SOCKET_EVENT = setCurrentCard(
             state.currentCard,
-            action.data,
+            [action.data],
           );
         }
       } else {
